fe3a2608d14db1df13f847637d2ed19497d6b5e3
newdich <newdichngr@gmail.com>
2025-07-31 11:36:14 +0100

    added files to command and query

---
+++ Config/Constant.php
@@ -0,0 +1,7 @@
+<?php
+include($_SERVER["DOCUMENT_ROOT"].'/Repository');
+const PAGE_PER_ROW = 50;
+const NEW_USER_STATUS ='active';
+const SUSPEND_STATUS ='suspended';
+const USER_SCHEMA = _USERS;
+?>
